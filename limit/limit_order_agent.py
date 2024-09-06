065605c7a958198bdbec8925f9575db0cba7bed3
mariaanitha.theobi <mariaanitha.theobi>
2024-09-06 13:18:26 +0530

    add limit order management implementation

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -1,16 +1,63 @@
 from trading_framework.execution_client import ExecutionClient
 from trading_framework.price_listener import PriceListener
 
-
 class LimitOrderAgent(PriceListener):
 
     def __init__(self, execution_client: ExecutionClient) -> None:
         """
-
         :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
         """
+        self.orders = []
         super().__init__()
+        self.execution_client = execution_client
+        
 
-    def on_price_tick(self, product_id: str, price: float):
+    def on_price_tick(self, product_id: str, price: float) -> None:
         # see PriceListener protocol and readme file
-        pass
+        """
+        Method executes whenever there is a new market price
+        @param product_id : The ID of the product
+        @param price : The new market price of the product
+        """
+        try:
+            for order in self.orders:
+                if order.get("product_id","") == product_id:
+                    amount = order.get("amount", 0)
+                    if order.get('action') == 'buy' and price <=amount:
+                        print(f"Buying {amount} of product {product_id}")
+                        self.execution_client.buy(product_id, amount)
+                        self.orders.remove(order)
+                    elif order.get('action') == 'sell' and price >= amount:
+                        print(f"Selling {amount} of product {product_id}")
+                        self.execution_client.buy(product_id, amount) 
+                        self.orders.remove(order)                       
+        except Exception as e:
+            print(f"Exception at on price tick : {e}")
+    
+    def add_order(self, action:bool, product_id:str, amount:int, limit:float) -> None:
+        """
+        Method to accept the order
+        @param is_buy : flag indicates buy or sell
+        @param product_id : id of the product
+        @param amount : the amount to buy or sell
+        @param limit : the limit at which to buy or sell
+
+        """
+        if action == '':
+            raise Exception("Provide the value as buy or sell")
+        elif product_id == '':
+            raise Exception("Provide the valid product id")
+        elif amount == '':
+            raise Exception("Provide the valid amount")
+        elif limit == '':
+            raise Exception("Provide the valid limit")
+        else:
+            self.orders.append({"action" : action,
+                                "product_id" :product_id,
+                                "amount" : amount,
+                                "limit" : limit 
+                                })
+
+
+
+
--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -1,7 +1,54 @@
 import unittest
+from unittest.mock import Mock
+import sys
+sys.path.insert(0, "../../")
+from limit.limit_order_agent import LimitOrderAgent
+from trading_framework.execution_client import ExecutionClient, ExecutionException
 
 class LimitOrderAgentTest(unittest.TestCase):
+    def executionclient(self):
+        self.exec_client = Mock(spec=ExecutionClient)
+        self.test_object = LimitOrderAgent(self.exec_client)
 
-    def test_something(self):
-        self.fail("not implemented")
+    def test_add_order(self):
+        self.executionclient()
+        self.test_object.add_order('buy', 'IBM', 1000, 100)
+        self.assertEqual(len(self.test_object.orders), 1)
+        self.assertEqual(self.test_object.orders[0], {'action': 'buy', 'product_id': 'IBM', 'amount': 1000, 'limit': 100})  #testing the orders added or not
+    
+    def test_add_order_validations(self):
+        self.executionclient()
+        with self.assertRaises(Exception):
+            self.test_object.add_order('', 'IBM', 1000, 100) #testing the exception for validation
 
+    def test_on_price_tick_for_buy(self):
+        self.executionclient()
+        self.test_object.add_order('buy', 'IBM', 1000, 100)
+        self.test_object.on_price_tick('IBM', 50)
+        self.assertEqual(len(self.test_object.orders), 0)  #testing the price tick for buy flag
+
+    def test_on_price_tick_for_sell(self):
+        self.executionclient()
+        self.test_object.add_order('sell', 'IBM', 1000, 100)
+        self.test_object.on_price_tick('IBM', 1100)
+        self.assertEqual(len(self.test_object.orders), 0)  #testing the price tick for sell flag
+
+
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
+    
+
+
+
+if __name__=="__main__":
+    test = LimitOrderAgent()
+    print(test.add_order(True,'d1',20000,2000))
+    print(test.add_order(False,'d2',12.12,12.12))
+    print(test.add_order(True,'d3',12111,12.12))
+    print(test.orders)
+    test.on_price_tick("d2", 13)
+    test.on_price_tick("d1", 1000)
